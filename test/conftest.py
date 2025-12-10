cb849150af1c3bdd644bc6069d974be3b62bd180
RICARDO <ricardomarquesddsa@gmail.com>
2025-12-09 22:52:14 -0300

    test:plan-and-client-test

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,10 @@
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from models import (
+    User, Client, Establishment, Customer, Employee, MarketingMessage, Payment, 
+    Service, Scheduling,StockProduct, StockMovement
+)
 
 @pytest.fixture(scope="session")
 def db_session():
@@ -11,3 +15,20 @@ def db_session():
     yield session
 
     session.close()
+
+@pytest.fixture(autouse=True)
+def clean_db(db_session):
+    yield
+    #db_session.query(Scheduling).delete()
+    #db_session.query(Customer).delete()
+    #db_session.query(Service).delete()
+    #db_session.query(Payment).delete()
+    #db_session.query(StockMovement).delete()
+    #db_session.query(StockProduct).delete()
+    #db_session.query(Payment).delete()
+    #db_session.query(MarketingMessage).delete()
+    #db_session.query(Employee).delete()
+    #db_session.query(Establishment).delete()
+    db_session.query(Client).delete()
+    db_session.query(User).delete()
+    db_session.commit()
--- test/test__db.py
+++ test/test__db.py
@@ -1,4 +1,3 @@
-import pytest
 from datetime import datetime
 from models import (
     User, UserRole, Plan, TypePlan, Client, Establishment, Customer, Employee,
@@ -19,3 +18,29 @@ def test_user_table(db_session):
     result = db_session.query(User).filter_by(user_name = "Test User").first()
     assert result is not None
     assert result.user_name == "Test User"
+
+def test_plan_table(db_session):
+    plans = db_session.query(Plan).all()
+    for plan in plans:
+        assert plan.type_plan in TypePlan
+
+def test_client_table(db_session):
+    user = User(
+            user_name="Client User",
+            password_hash="hashed",
+            phone_number="987654321",
+            email="client@example.com",
+            role=UserRole.CLIENT,
+            active_status=True
+        )
+    db_session.add(user)
+    db_session.commit()
+
+    user_result = db_session.query(User).filter_by(user_name = "Client User").first()
+    client = Client(users_id = user_result.id, plans_id = 1)
+    db_session.add(client)
+    db_session.commit()
+
+    client_result = db_session.query(Client).filter_by(users_id = client.users_id).first()
+    assert client_result.plans_id == 1
+    assert client_result.users_id == user_result.id
